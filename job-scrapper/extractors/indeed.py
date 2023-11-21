4c8c9190fe99cbd5a840bdbdb325af22d4234213
ktbyu823op <ktb823op@naver.com>
2023-11-21 21:46:35 +0900

    Finished job-scrapper

---
+++ job-scrapper/extractors/indeed.py
@@ -0,0 +1,51 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+
+def get_page_count(keyword):
+    browser = webdriver.Chrome()
+    base_url = "https://kr.indeed.com/jobs?q="
+    browser.get(f"{base_url}{keyword}")
+
+    soup = BeautifulSoup(browser.page_source, "html.parser")
+    pagination=soup.find("ul",class_="css-1g90gv6 eu4oa1w0")
+   
+    pages=pagination.find_all("li",recursive=False)
+    count = len(pages)
+    if count==0:
+        return 1
+    elif count>5:
+        return 5
+    else:
+        return count-1
+    
+def extract_indeed_jobs(keyword):
+    pages=get_page_count(keyword)
+    print("Found",pages,"pages")
+    results = []
+    for page in range(pages):
+        browser = webdriver.Chrome()
+
+        base_url = "https://kr.indeed.com/jobs"
+        final_url=f"{base_url}?q={keyword}&start={page*10}"
+        print("Requesting",final_url)
+        browser.get(final_url)
+
+        soup = BeautifulSoup(browser.page_source, "html.parser")
+        job_list = soup.find("ul", class_="css-zu9cdh eu4oa1w0")
+        jobs = job_list.find_all("li", recursive=False)
+        for job in jobs:
+            zone = job.find("div", class_="mosaic-zone")
+            if zone == None:
+                anchor = job.select_one("h2 a")
+                title = anchor["aria-label"]
+                link = anchor["href"]
+                company = job.find("span", class_="css-1x7z1ps eu4oa1w0")
+                location = job.find("div", class_="css-t4u72d eu4oa1w0")
+                job_data = {
+                    "link": f"https://kr.indeed.com{link}",
+                    "company": company.string.replace(","," "),
+                    "location": location.string.replace(","," "),
+                    "position": title.replace(","," "),
+                }
+                results.append(job_data)
+    return results
--- job-scrapper/job-scrapper.py
+++ job-scrapper/job-scrapper.py
@@ -1,32 +1,16 @@
-from selenium import webdriver
-from bs4 import BeautifulSoup
 from extractors.wwr import extract_wwr_jobs
+from extractors.indeed import extract_indeed_jobs
 
-browser = webdriver.Chrome()
+keyword = input("What do you want to search for?")
 
-base_url = "https://kr.indeed.com/jobs?q="
-search_term = "python"
-browser.get(f"{base_url}{search_term}")
+indeed = extract_indeed_jobs(keyword)
+wwr=extract_wwr_jobs(keyword)
+jobs= indeed + wwr
 
-results = []
-soup = BeautifulSoup(browser.page_source, "html.parser")
-job_list = soup.find("ul", class_="css-zu9cdh eu4oa1w0")
-jobs = job_list.find_all("li", recursive=False)
+file=open(f"{keyword}.csv","w",encoding="utf-8-sig")
+file.write("Position,Company,Location,URL\n")
+
 for job in jobs:
-    zone = job.find("div", class_="mosaic-zone")
-    if zone == None:
-        anchor = job.select_one("h2 a")
-        title = anchor["aria-label"]
-        link = anchor["href"]
-        company = job.find("span", class_="css-1x7z1ps eu4oa1w0")
-        location = job.find("div", class_="css-t4u72d eu4oa1w0")
-        job_data = {
-            "link": f"https://kr.indeed.com{link}",
-            "company": company.string,
-            "location": location.string,
-            "position": title,
-        }
-        results.append(job_data)
-for result in results:
-    print(result)
-    print("---------------------")
+    file.write(f"{job['position']},{job['company']},{job['location']},{job['link']}\n")
+
+file.close()
